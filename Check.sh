41b270550018c19f4dd8828865dc3e3dbc441634
Atiger77 <atiger77@126.com>
2016-08-11 15:12:33 +0800

    change variable #30 增加环境版本，以免出现变量获取失败

--- Check.sh
+++ Check.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Author: atiger77
+# Date: 2016/08/11
+# Version: V1.2
 # Desc: This script will check Dionaea.log's md5.
 #       Rember add this  script to crontab.
 #       */5 * * * * /bin/bash /xxx(script-location)/Check.sh
@@ -5,14 +9,19 @@
 # Define
 Log_location="/opt/Dionaea.log"
 Check_MD5_location="/opt/dionaea_md5check.txt"
-Alarm_Email="atiger77@xxxx"
+Alarm_Email="yyy@xxx.com"
 Dionaea_Hostname=`hostname`
-Dionaea_HostIP=`ifconfig eth0 | sed -n 2p | awk  '{ print $2 }' `
+Dionaea_HostIP_Centos7=`ifconfig eth0 | sed -n 2p | awk  '{ print $2 }'`
+Dionaea_HostIP_Centos6=`ifconfig eth0 |awk -F '[ :]+' 'NR==2 {print $4}'`
+Dionaea_HostIP_Ubuntu=`ifconfig eth0 |awk -F '[ :]+' 'NR==2 {print $4}'`
+Dionaea_HostIP=$Dionaea_HostIP_Centos7
+
 
 # Check MD5 file.
 if [ ! -s $Check_MD5_location ]; then
     echo `/usr/bin/md5sum $Log_location` > $Check_MD5_location
 fi
+
 
 # Contrast file.
 Old_md5=`awk '{print $1}' $Check_MD5_location`
@@ -21,5 +30,4 @@ if [ $Old_md5 == $New_md5 ]; then
     sleep 0.5
 else
     /usr/bin/md5sum $Log_location > $Check_MD5_location
-    echo -e "Error,Please check \nhostname:$Dionaea_Hostname hostip:$Dionaea_HostIP" | mailx -s "Web_Dionaea logs was changed!" $Alarm_Email
-fi
+    echo -e "Dionaea logs changed!!! Please check: \nhostname:$Dionaea_Hostname hostip:$Dionaea_HostIP" | mailx -s "Web_Dionaea logs was changed!" $Alarm_Email 
